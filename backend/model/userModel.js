1b45b6433df046ce7280993ef647451dfe8d5e9d
Sharayu Shelke <sharayushelke2006@gmail.com>
2026-05-25 02:18:36 +0530

    Refactor user model to include wishlist field

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -6,45 +6,35 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
     },
+
     password: {
       type: String,
       required: true,
     },
+
     cartData: {
       type: Object,
       default: {},
     },
+
+    wishlist: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
   },
   {
     timestamps: true,
-    minimize: false, // Prevents empty nested objects from being removed
-  },
-const userSchema = new mongoose.Schema({
-  password: {
-    type: String,
-    required: true
-  },
-
-  cartData: {
-    type: Object,
-    default: {}
-  },
-
-  wishlist: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product"
-    }
-  ]
-}, { 
-  timestamps: true,
-  minimize: false
-});
+    minimize: false, // prevents empty objects from being removed
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
