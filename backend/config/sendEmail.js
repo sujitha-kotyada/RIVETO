4708c5994b9532ff7fc13bfb1268aeca233b7c4f
Sharayu Shelke <sharayushelke2006@gmail.com>
2026-07-06 18:51:13 +0530

    Implement clear cart functionality in backend and frontend

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -6,8 +6,8 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    // user: process.env.EMAIL_USER,
-    // pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
